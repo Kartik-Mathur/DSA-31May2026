682da4d34a4d54e1928eb4c7c9eb90b43e5d6111
Kartik <kartik.mathur10@gmail.com>
2026-08-08 13:17:47 +0530

    Recursion and LL

---
+++ L21-OOPS/Test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	const int a = 10;
+	// a = 11;
+
+	cout << a << endl;
+
+	int x = 10; // initialisation
+
+	int y;
+	y = x; // assignment 
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
